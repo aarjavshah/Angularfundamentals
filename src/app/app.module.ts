9d3ca60b3955ed8b1ccaec38f6dcb518bad59e9e
aarjavshah <arjav.d.shah@gmail.com>
2019-03-12 16:09:48 +0100

    starting with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { EventListComponent } from './events/event-list.component';
 import { EventThumbComponent } from './events/event-thumbnai.component';
+import { EventDetailsComponent } from './events/event-details.component';
 import { MyAppComp } from './app.component';
 import {NavigationComponent} from './nav/navbar.component';
 import {EventService} from './events/shared/event.service';
 import {ToastrService} from './common/toastr.service'
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+
 @NgModule({
   declarations: [
     MyAppComp,
     EventListComponent,
     EventThumbComponent,
-    NavigationComponent
+    NavigationComponent,
+    EventDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [EventService,ToastrService],
   bootstrap: [MyAppComp]
